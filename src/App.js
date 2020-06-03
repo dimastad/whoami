3c05ddce695067933a2cb1c704765bb263a67d8b
dimastad <dmitrii_portnov@mail.ru>
2020-06-03 16:39:15 +0400

    create app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Whoami
+      </header>
+      <main className="App-main">
+        Something...
+      </main>
+    </div>
+  );
+}
+
+export default App;
