eb02fa2f8611227f035877fb5d8fd991161085f1
dimastad <dmitrii_portnov@mail.ru>
2020-09-11 09:50:39 +0400

    Updates

---
+++ precache-manifest.689e1f7cd2bc3cd353c8538782de48ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865a9cb1d849247a9d6ec958367892fe",
+    "url": "/whoami/index.html"
+  },
+  {
+    "revision": "a7050fed708ecd3c2601",
+    "url": "/whoami/static/css/main.bdd4cfc3.chunk.css"
+  },
+  {
+    "revision": "d62e0263fea540d59dc5",
+    "url": "/whoami/static/js/2.76d6f2f8.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/whoami/static/js/2.76d6f2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7050fed708ecd3c2601",
+    "url": "/whoami/static/js/main.5feec899.chunk.js"
+  },
+  {
+    "revision": "2e1cd578dc72c6eb9693",
+    "url": "/whoami/static/js/runtime-main.31d2179f.js"
+  }
+]);
