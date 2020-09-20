600cea14c8405ab6d0424b011ae55136cf42dd8c
dimastad <dmitrii_portnov@mail.ru>
2020-09-20 23:16:13 +0400

    add 15puzzle

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,12 +3,33 @@ import './Game.scss'
 
 export default () => {
 
+  const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, '']
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    const emptyCell = document.getElementsByClassName('emptyCell')
+    const currentOrder = e.target.style.order
+    const currentEmptyCell = emptyCell[0].style.order
+    
+    emptyCell[0].style.order = currentOrder
+    e.target.style.order = currentEmptyCell
+  }
+
   return (
-    <>
-      <iframe 
-        title='puzzle'
-        src = "https://15puzzle.netlify.app/"
-        seamless />
-    </>
+    <div className="playField">
+      {cells
+        .sort(()=>Math.random() - 0.5)
+        .map((cell, index) => (
+        <div 
+          style={{ order: index }}
+          className={cell.length === 0 
+            ? 'emptyCell'
+            : 'cell'} 
+          key={cell}
+          onClick={handleClick}>
+            {cell}
+        </div>
+      ))}
+    </div>
   )
 }
